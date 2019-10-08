1a8b553a76fba127cc4dba88db703dd7e83fe36c
dano5342 <ellis.daniel0@gmail.com>
2019-10-08 12:52:08 +0200

    Have added routing for About/Contact/Home. updated settings for joining static files

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import contact_view, about_view
 
 urlpatterns = [
-    path('about/', about_view),
-    path('contact/', contact_view) 
+    path('about/', about_view, name="about"),
+    path('contact/', contact_view, name="contact") 
 ]
