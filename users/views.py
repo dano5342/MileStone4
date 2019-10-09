8905b7fce288a723b988b96722d0ff2c332b51f0
dano5342 <ellis.daniel0@gmail.com>
2019-10-09 11:37:39 +0200

    registration now 100%, base html title amended navbar, views changed to custom form

--- users/views.py
+++ users/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UserRegistrationForm
 
 def register(request):
     """ 
     Registers a new user on the backend
     """
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'register.html', {'form': form})
