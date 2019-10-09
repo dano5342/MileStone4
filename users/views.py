0d278a791364abfde65cadf7a73611e48a515551
dano5342 <ellis.daniel0@gmail.com>
2019-10-09 11:24:21 +0200

    registration redir now works, returns user to homepage. Not currently storing users. Directories and files added for SCSS and CSS

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
     """ 
     Registers a new user on the backend
     """
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
