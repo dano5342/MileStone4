cbee446d328b47a11baf10d98248997c0b82be35
dano5342 <ellis.daniel0@gmail.com>
2019-10-09 10:31:18 +0200

    amended base.html and views.py for products to try and show products on the main page. New accounts app added and will be worked on

--- ms4/urls.py
+++ ms4/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import urlpatterns as pageviews
 from pages.views import home_view
-
+from products.views import all_products
 
 urlpatterns = [
-    path('', home_view),
+    path('', home_view, name="home"),
     path('admin/', admin.site.urls),
-    path('pageviews/', include(pageviews))
-
+    path('pageviews/', include(pageviews)),
+    path('products/', all_products, name="products")
 ]
