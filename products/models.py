d60a2038d8bece875ffdc96a64657c57c431bd56
dano5342 <ellis.daniel0@gmail.com>
2019-10-12 11:36:11 +0200

    messing with product.models to try and get them displaying. Will keep at this for a while before testing again.

--- products/models.py
+++ products/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Product(models.Model):
-    name        = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
     description = models.TextField(blank=False, null=False)
-    price       = models.DecimalField(max_digits=6, decimal_places=2)
-    summary     = models.TextField(default="This is a test")
-    featured    = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    summary = models.TextField(default="This is a test")
+    featured = models.BooleanField(default=True)
+    posted_at = models.DateTimeField(default=timezone.now)
