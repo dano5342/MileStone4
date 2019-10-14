ab5d02df8e40a4e779b75a194e950070971cb5d5
dano5342 <ellis.daniel0@gmail.com>
2019-10-14 15:26:20 +0200

    added a product.html to view a single product, not quite working yet. urls.py added into users for routing account settings. Views.py added edit profile view, this needs wiring up to the new editProfile page

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
 
@@ -21,3 +22,16 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'profile.html')
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save
+            return redirect('profile.html')
+    else:
+        form = UserChangeForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'edit_profile.html', args)
